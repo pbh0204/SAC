9bdf78cc768c7eaf625b8c6d1e6582574e2c164b
pbh0204 <166804963+pbh0204@users.noreply.github.com>
2024-09-12 13:59:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -35,5 +35,5 @@ async render () {
   }
   this._root.textContent = JSON.stringify(dataBinding)
 }  
-customElements.define('com-sap-sac-exercise-pbh-main', Main)
+customElements.define('com-sap-sac-exercise-pbk-main', Main)
 })()
