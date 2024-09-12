58a96aadfc7f881cdae971bf7f1ba52bb77c5cd8
pbh0204 <166804963+pbh0204@users.noreply.github.com>
2024-09-12 14:14:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
+
 (function () {
-const template = document.createElement('template')
-template.innerHTML = `
-<style>
-</style>
-<div id="root" style="width: 100%; height: 100%;">
-Hello WebComponent
-</div>
-`
-class Main extends HTMLElement {
-constructor () {
-super()
-this._shadowRoot = this.attachShadow({ mode: 'open' })
-this._shadowRoot.appendChild(template.content.cloneNode(true))
-this._root = this._shadowRoot.getElementById('root')
-}
-}
+  const template = document.createElement('template')
+  template.innerHTML = `
+        <style>
+        </style>
+        <div id="root" style="width: 100%; height: 100%;">
+        Hello Custom Widget
+        </div>
+      `
+  class Main extends HTMLElement {
+    constructor () {
+      super()
 
-onCustomWidgetResize (Width, height) {
-  this.render()
-}
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(template.content.cloneNode(true))
 
-onCustomWidgetAfterUpdate (changedProps) {
-  
-}
+      this._root = this._shadowRoot.getElementById('root')
+    }
 
-onCustomWidgetDestroy () {
-  
-}
+    onCustomWidgetResize (width, height) {
+      this.render()
+    }
 
-async render () {
-  const dataBinding = this.dataBinding
-  if (!dataBinding || dataBinding.state !== 'success') {
-    return
+    onCustomWidgetAfterUpdate (changedProps) {
+      this.render()
+    }
+
+    onCustomWidgetDestroy () {
+    }
+      async render () {
+        const dataBinding = this.dataBinding
+        if (!dataBinding || dataBinding.state !== 'success') {
+           return
+         }
+         this._root.textContent = JSON.stringify(dataBinding)
+      }
   }
-  this._root.textContent = JSON.stringify(dataBinding)
-}  
-customElements.define('com-sap-sac-exercise-pba-main', Main)
+
+  customElements.define('com-sap-sac-exercise-pbs-main', Main)
 })()
